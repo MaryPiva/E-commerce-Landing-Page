a8f16cd5dd8fded459a44cbcc9f4a205415c3bbc
MaryPiva <mari.devpro@gmail.com>
2025-07-02 17:19:06 +0100

    .

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Troca a imagem principal ao clicar nas miniaturas
 function slider(imageSrc) {
   document.getElementById('main-image').src = imageSrc;
 }
 
-// Menu hambúrguer toggle
-let menu = document.getElementById('menu');
-let menuIcon = document.querySelector('#menu i');
-let navbar = document.querySelector('.navegacao');
+const menu = document.querySelector('#menu');
+const menuIcon = document.querySelector('#menu i');
+const navbar = document.querySelector('.navegacao');
 
+// Abrir/fechar menu
 menu.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('open');
 };
+
+// Fechar menu ao clicar em um link
+document.querySelectorAll('.navegacao a').forEach(link => {
+  link.onclick = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('open');
+  };
+});
